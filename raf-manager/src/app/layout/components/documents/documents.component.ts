6177b4a962da3b132026bb99c74e363484b03d54
nmfeka <nkosinathi.mfeka@fnb.co.za>
2018-01-10 22:33:43 +0200

    added the documents component.

---
+++ raf-manager/src/app/layout/components/documents/documents.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { BookingsService, IDocument} from '../../../services/bookings.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-documents',
+  templateUrl: './documents.component.html',
+  styleUrls: ['./documents.component.scss'],
+  providers: [BookingsService, NgbModal, FormsModule, ReactiveFormsModule,],
+})
+export class DocumentsComponent implements OnInit {
+  documents: IDocument[] = [];
+  document: IDocument = this.initialiseDocument();
+
+  constructor(private bookingsService: BookingsService, private modalService: NgbModal) { }
+  
+  initialiseDocument() {
+    return {Id:0,BookingId:0, DocumentType: '',DocumentName: '',DocumentExtension: '', Contents: new Blob()}
+  }
+
+  deleteDocument(document: IDocument) {
+    this.bookingsService.deleteDocument(document.Id)
+    .subscribe(o => {
+        this.getDocuments(document.BookingId);
+    },error => console.log("Error :: " + error))
+  }
+
+  ngOnInit() {
+  }
+
+getDocuments(bookingId) {
+   this.bookingsService.getDocuments(bookingId)
+        .subscribe(results => this.documents = results,
+            error => console.log("Error :: " + error));
+}
+
+uploadDocument() {
+  //save documents                
+  if(this.document.DocumentName != null || this.document.DocumentName != ''){
+      this.document.BookingId = this.booking.Id;
+      this.bookingsService.saveDocument(this.document).subscribe(a => {
+          this.documents.push(this.document);
+          this.document = this.initialiseDocument();        
+      },error => console.log("Error :: " + error));
+  }                
+}
+
+onFileChange(event) {
+    let reader = new FileReader();
+    if(event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];          
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.document.DocumentName = file.name;
+        this.document.DocumentExtension = file.type;
+        this.document.Contents = reader.result.split(',')[1];
+      };          
+    }
+  }
+
+}

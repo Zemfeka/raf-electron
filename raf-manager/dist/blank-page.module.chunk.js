4b54f2e1816421b124ed0ba1ec09837c7aeb76fc
nmfeka <nkosinathi.mfeka@fnb.co.za>
2018-01-15 10:45:37 +0200

    cleaning up the header and removing menu itmes

---
+++ raf-manager/dist/blank-page.module.chunk.js
@@ -0,0 +1,139 @@
+webpackJsonp(["blank-page.module"],{
+
+/***/ "../../../../../src/app/layout/blank-page/blank-page-routing.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return BlankPageRoutingModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("../../../router/esm5/router.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__blank_page_component__ = __webpack_require__("../../../../../src/app/layout/blank-page/blank-page.component.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var routes = [
+    {
+        path: '',
+        component: __WEBPACK_IMPORTED_MODULE_2__blank_page_component__["a" /* BlankPageComponent */]
+    }
+];
+var BlankPageRoutingModule = (function () {
+    function BlankPageRoutingModule() {
+    }
+    BlankPageRoutingModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+            imports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["c" /* RouterModule */].forChild(routes)],
+            exports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["c" /* RouterModule */]]
+        })
+    ], BlankPageRoutingModule);
+    return BlankPageRoutingModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/blank-page/blank-page.component.html":
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/blank-page/blank-page.component.scss":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/blank-page/blank-page.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return BlankPageComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var BlankPageComponent = (function () {
+    function BlankPageComponent() {
+    }
+    BlankPageComponent.prototype.ngOnInit = function () { };
+    BlankPageComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+            selector: 'app-blank-page',
+            template: __webpack_require__("../../../../../src/app/layout/blank-page/blank-page.component.html"),
+            styles: [__webpack_require__("../../../../../src/app/layout/blank-page/blank-page.component.scss")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], BlankPageComponent);
+    return BlankPageComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/blank-page/blank-page.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "BlankPageModule", function() { return BlankPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__("../../../common/esm5/common.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__blank_page_routing_module__ = __webpack_require__("../../../../../src/app/layout/blank-page/blank-page-routing.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__blank_page_component__ = __webpack_require__("../../../../../src/app/layout/blank-page/blank-page.component.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+var BlankPageModule = (function () {
+    function BlankPageModule() {
+    }
+    BlankPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+            imports: [__WEBPACK_IMPORTED_MODULE_1__angular_common__["b" /* CommonModule */], __WEBPACK_IMPORTED_MODULE_2__blank_page_routing_module__["a" /* BlankPageRoutingModule */]],
+            declarations: [__WEBPACK_IMPORTED_MODULE_3__blank_page_component__["a" /* BlankPageComponent */]]
+        })
+    ], BlankPageModule);
+    return BlankPageModule;
+}());
+
+
+
+/***/ })
+
+});
+//# sourceMappingURL=blank-page.module.chunk.js.map
